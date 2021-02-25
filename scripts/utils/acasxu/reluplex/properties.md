7f76dcad2442d6dd7840b19969c0e71b471d37ca
shchae7_server <shchae7@postech.ac.kr>
2021-02-26 03:32:36 +0900

    reluplex acasxu run_prop scripts added

---
+++ scripts/utils/acasxu/reluplex/properties.md
@@ -0,0 +1,1 @@
+For properties 4 ~ 10, need to modify the main.cpp files in check_properties/propertyi directory
---
+++ scripts/utils/acasxu/reluplex/run_property4.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+TIMEOUT=12h
+
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_1_1.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_1_1.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_1_2.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_1_2.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_1_3.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_1_3.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_1_4.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_1_4.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_1_5.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_1_5.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_1_6.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_1_6.txt
+
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_2_1.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_2_1.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_2_2.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_2_2.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_2_3.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_2_3.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_2_4.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_2_4.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_2_5.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_2_5.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_2_6.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_2_6.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_2_7.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_2_7.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_2_8.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_2_8.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_2_9.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_2_9.txt
+
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_3_1.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_3_1.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_3_2.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_3_2.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_3_3.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_3_3.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_3_4.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_3_4.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_3_5.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_3_5.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_3_6.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_3_6.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_3_7.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_3_7.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_3_8.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_3_8.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_3_9.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_3_9.txt
+
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_4_1.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_4_1.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_4_2.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_4_2.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_4_3.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_4_3.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_4_4.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_4_4.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_4_5.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_4_5.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_4_6.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_4_6.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_4_7.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_4_7.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_4_8.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_4_8.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_4_9.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_4_9.txt
+
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_5_1.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_5_1.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_5_2.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_5_2.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_5_3.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_5_3.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_5_4.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_5_4.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_5_5.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_5_5.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_5_6.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_5_6.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_5_7.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_5_7.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_5_8.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_5_8.txt
+timeout --foreground --signal=SIGQUIT $TIMEOUT ./check_properties/bin/property4.elf ./nnet/ACASXU_experimental_v2a_5_9.nnet logs/property4_summary.txt 2>&1 | tee logs/property4_stats_5_9.txt
